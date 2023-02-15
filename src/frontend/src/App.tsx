8c22fcd0eedd5d882092bd18d1c4ba1024ebac2f
murtuza syed <murtuzasyed@MacBook-Pro.local>
2023-02-15 13:26:30 -0600

    Fix ts errors

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,18 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
   PlusOutlined,
-  TeamOutlined,
-  UserOutlined,
 } from "@ant-design/icons";
-import type { MenuProps } from "antd";
 import {
   Button,
   Layout,
-  Menu,
   Spin,
   Table,
   Tag,
@@ -32,36 +25,7 @@ export type StudentData = {
   email: string;
   location: string;
 };
-type MenuItem = Required<MenuProps>["items"][number];
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
-const { Header, Content, Footer, Sider } = Layout;
-const items: MenuItem[] = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
-];
-
+const { Header, Content, Footer } = Layout;
 const tableColumns = (onStudentsUpdated: () => void) => [
   {
     title: "",
@@ -121,7 +85,6 @@ const tableColumns = (onStudentsUpdated: () => void) => [
   },
 ];
 function App() {
-  const [collapsed, setCollapsed] = useState(false);
   const [students, setStudents] = useState<StudentData[]>([]);
   const [loading, setLoading] = useState(false);
   const [showAddStudentDrawer, setShowAddStudentDrawer] = useState(false);
