5885e58c15150d00a8496be1fc10ef5beec1c36f
Murtuza Syed <murtuzasyed1987@gmail.com>
2023-02-21 11:06:28 -0600

    Chore: Cleanup

--- src/frontend/src/actions/types.ts
+++ src/frontend/src/actions/types.ts
@@ -1,4 +1,5 @@
-export const CREATE_STUDENT="CREATE_STUDENT";
-export const DELETE_STUDENT="DELETE_STUDENT";
-export const UPDATE_STUDENT="UPDATE_STUDENT";
-export const GET_STUDENTS="GET_STUDENTS";
+// Old Redux implementation
+export const CREATE_STUDENT = "CREATE_STUDENT";
+export const DELETE_STUDENT = "DELETE_STUDENT";
+export const UPDATE_STUDENT = "UPDATE_STUDENT";
+export const GET_STUDENTS = "GET_STUDENTS";
